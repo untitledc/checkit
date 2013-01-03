94e2cfe274f893073d9b74a6b07e0f67f1c9eb36
untitled c <untitled.c@gmail.com>
2013-01-04 02:36:11 +0800

    Returns dict result to tab by re-structuring and introducing jQuery.
    jQuery is used to parse the HTML output from Yahoo dictionary.
    I think I will use it to show the preview tooltip as well.
    
    background.js and content.js are also re-designed.
    content.js simply informs background.js what's been done in this tab.
    It no longer asks background.js to "newtab".  Instead, it indicates it's a
    selection event, and what's been selected, which key has been holded during selection,
    what are the current tab url, etc.
    
    background.js handles most of the logic, including determining which BE to audit,
    which action to do (new tab or show preview), and telling content.js what to show in preview.
    
    This commit allows users to mouse-select (still holding 'c')
    and it will hit Yahoo dictionary, parse the title of the first word,
    and return to content.js some information.  The rendering of tooltip
    and the parsing of explanation of the word are not done in this commit.

--- scripts/background.js
+++ scripts/background.js
@@ -1,8 +1,8 @@
 chrome.extension.onMessage.addListener(
     function(message, sender, callback) {
-        switch(message.action) {
-            case "newtab":
-                newTab(message.param.url);
+        switch ( message.message ) {
+            case "select":
+                handleSelection(message, sender.tab.id);
                 break;
         }
     }
@@ -12,4 +12,53 @@ function newTab(url) {
     chrome.tabs.create({
         "url": url
     });
+}
+
+function handleSelection(message, tabid) {
+    // I should do some logic here
+    // backend, backendUrl and action will be determined by
+    // options, keypressed, url (context) ...
+    // for now let me slack a bit
+    var backendUrl = "http://tw.dictionary.search.yahoo.com/search?p="+message.query;
+    var backend = "dict.s.y.c";
+    var action = "preview"
+
+    $.get(backendUrl, function(html, textStatus) {
+            handleBEOutput(html,textStatus,backendUrl,backend,action,tabid);
+        });
+}
+
+/**
+ * backendUrl: the backend url we request html from
+ * backend: some string indicating the backend (e.g. "dict.s.y.c", "imdb");
+ * action: "preview" or "opentab"
+ * tabid: the (chrome) tab id from which the request was fired.
+ *     background will send back processed BE output for preview.
+ */
+function handleBEOutput(html, textStatus, backendUrl, backend, action, tabid) {
+    if ( textStatus == "success" ) {
+        if ( action == "preview" ) {
+            var previewParams = {};
+            switch (backend) {
+                case "dict.s.y.c":
+                    previewParams = ydictPreviewParser(html);
+                    previewParams.moreurl = backendUrl;
+                    chrome.tabs.sendMessage(tabid,
+                            {"action":action, "preview":previewParams});
+                    break;
+            }
+        }
+    }
+    else {
+        // something's wrong in the connection
+        console.log("error when fetching "+backend);
+    }
+}
+
+function ydictPreviewParser(html) {
+    var title = ($(html).find(".title_term").first().find(".yschttl").text());
+    // I'm not familiar with jQuery, let me slack a bit
+    var desc = "omgomgomg";
+
+    return {"title": title, "desc": desc};
 }
--- scripts/content.js
+++ scripts/content.js
@@ -1,22 +1,59 @@
-window.onmouseup=null;
-function check(e) {
-	var str = window.getSelection().toString();
-	if ( str ) {
-        var url="http://tw.dictionary.search.yahoo.com/search?p="+str;
-        chrome.extension.sendMessage({
-            "action": "newtab",
-            "param": {
-                "url": url
-            }
-        });
-	}
+var keyDownCode = -1;
+// some keys are not detected since they might be used when doing selection
+// for example holding shift and end to select till the end of the line
+// (although I'm currently using mouse-selection as triggering)
+// http://www.w3.org/2002/09/tests/keys.html
+// SHIFT,HOME,END,PAGEUP,PAGEDOWN,4 arrow keys
+var IGNORING_KEYCODES = [16,36,35,33,34,37,38,39,40];
+
+/**
+ * response: a response object from background.js
+ *     containing what to do in this (chrome) tab
+ */
+function handleSelection(response) {
+    console.log(response);
+    var action = response.action;
+    if ( action == "preview" ) {
+        console.log(response.preview);
+    }
+}
+chrome.extension.onMessage.addListener(
+    function(message,sender,callback) {
+        handleSelection(message);
+    }
+);
+
+window.onmouseup = function(e) {
+    if ( keyDownCode == 67 ) {
+        var str = window.getSelection().toString();
+        if ( str ) {
+            chrome.extension.sendMessage({
+                "message": "select",
+                "url": window.location.href,
+                "query": str,
+                "keycode": keyDownCode
+            });
+        }
+    }
 }
 
+
 window.onkeydown=function(e){
-	if ( e.keyCode == 67 ) {
-		window.onmouseup=check;
-	}
+    if ( ! arrayContains(IGNORING_KEYCODES, e.keyCode) ) {
+        keyDownCode = e.keyCode;
+    }
 }
 window.onkeyup=function(e){
-	window.onmouseup=null;
+    if ( ! arrayContains(IGNORING_KEYCODES, e.keyCode) ) {
+        keyDownCode = -1;
+    }
+}
+
+function arrayContains(a, obj) {
+    for (var i = 0; i < a.length; i++) {
+        if (a[i] === obj) {
+            return true;
+        }
+    }
+    return false;
 }
